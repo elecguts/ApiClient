d91098b9461a6ac12c26731904ca5de5b8e9205b
Samuel Gonzalez <Nomekop65@yahoo.com>
2023-05-09 21:24:57 -0400

    Very basic API interaction working using streaming and Joke class structure

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        public class Joke
+        {
+            [JsonPropertyName("type")]
+            public string Type { get; set; }
+
+            [JsonPropertyName("setup")]
+            public string Setup { get; set; }
+
+            [JsonPropertyName("punchline")]
+            public string PunchLine { get; set; }
+
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+        }
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_joke");
+
+            var randomJoke = await JsonSerializer.DeserializeAsync<Joke>(responseAsStream);
+
+            Console.WriteLine(randomJoke.Setup);
+            Console.Read();
+            Console.WriteLine(randomJoke.PunchLine);
+        }
     }
-  }
 }
